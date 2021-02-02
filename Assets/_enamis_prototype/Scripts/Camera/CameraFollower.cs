2fb9bce05fab465947f42bac5ed3b9b299e4194e
Neil <neil.gaillard@hotmail.com>
2021-02-02 17:08:02 +0100

    Fixed camera issues

--- Assets/_enamis_prototype/Scripts/Camera/CameraFollower.cs
+++ Assets/_enamis_prototype/Scripts/Camera/CameraFollower.cs
@@ -4,18 +4,24 @@ namespace _enamis_prototype.Scripts.Camera
 {
     public class CameraFollower : MonoBehaviour
     {
+        private Transform _playerTransform;
+        private Transform _cameraTransform;
+
+        private Vector3 _targetPosition;
         private readonly Vector3 _offset = new Vector3(0, 2, -10);
-    
+
         // Start is called before the first frame update
         void Start()
         {
+            _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+            _cameraTransform = transform;
         }
 
         // LateUpdate is called once per frame
         void LateUpdate()
         {
-            transform.position += _offset;
+            _targetPosition = _playerTransform.position + _offset;
+            _cameraTransform.position = _targetPosition;
         }
-
     }
 }
--- Assets/_enamis_prototype/Scripts/Player/PlayerController.cs
+++ Assets/_enamis_prototype/Scripts/Player/PlayerController.cs
@@ -4,19 +4,19 @@ namespace _enamis_prototype.Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        private Rigidbody2D _playerRigidbody2D;
+        
         private float _speed = 0.2f;
-        private float _jumpForce = 1.0f;
+        private float _jumpForce = 120.0f;
 
-        private bool isAlive = true;
-        private bool isOnGround;
-        private bool isDashing;
+        private bool _isOnGround = true;
 
         private float _horizontalInput;
         
         // Start is called before the first frame update
         void Start()
         {
-        
+            _playerRigidbody2D = GetComponent<Rigidbody2D>();
         }
 
         // FixedUpdate is called once per frame
@@ -24,7 +24,35 @@ namespace _enamis_prototype.Scripts.Player
         {
             _horizontalInput = Input.GetAxis("Horizontal");
 
-            transform.Translate(Vector3.right * (_speed * _horizontalInput));
+            Move();
+            
+            if (_isOnGround && Input.GetKeyDown(KeyCode.Space))
+                Jump();
+        }
+
+        private void Move()
+        {
+            Vector3 newAngle = new Vector3(0, 0, -5);
+            Transform transform1;
+            
+            (transform1 = transform).Translate(Vector3.right * (_speed * _horizontalInput));
+            transform1.eulerAngles = newAngle * _horizontalInput;
+        }
+
+        private void Jump()
+        {
+            _playerRigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+
+            transform.localScale += new Vector3(0, -0.5f, 0);
+            transform.localScale += new Vector3(0, 0.5f, 0);
+            
+            _isOnGround = false;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+                _isOnGround = true;
         }
     }
 }
