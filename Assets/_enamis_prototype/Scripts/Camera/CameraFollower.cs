14d3a0e0e4d87b95e70ea24a56c2c06b9da79925
Neil <neil.gaillard@hotmail.com>
2021-02-03 08:40:25 +0100

    Fixed Jump, Texture, Camera Movements, Added Tokens

--- Assets/_enamis_prototype/Scripts/Camera/CameraFollower.cs
+++ Assets/_enamis_prototype/Scripts/Camera/CameraFollower.cs
@@ -1,27 +1,84 @@
+using System;
 using UnityEngine;
 
 namespace _enamis_prototype.Scripts.Camera
 {
     public class CameraFollower : MonoBehaviour
     {
+        // ------Constants ------
+        private const float XPosBase = 0.0f;
+        private const float YPosBase = 5.0f;
+        private const float ZPosBase = -10.0f;
+        
+        // ------ References ------
         private Transform _playerTransform;
         private Transform _cameraTransform;
 
-        private Vector3 _targetPosition;
-        private readonly Vector3 _offset = new Vector3(0, 2, -10);
+        // ------ Private Attributes ------
+        private Vector3 _targetPosition = new Vector3(0, 0, 0);
+        private readonly Vector3 _offset = new Vector3(XPosBase, YPosBase, ZPosBase);
+
+        private FollowMode _followMode = FollowMode.Basic;
+        
+        private enum FollowMode
+        {
+            Basic,
+            ReajustHeight,
+            GoingDown
+        }
+        
+        // ------ Event Methods ------
 
         // Start is called before the first frame update
-        void Start()
+        private void Awake()
         {
             _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
             _cameraTransform = transform;
         }
 
+        private void Update()
+        {
+            _followMode = UpdateFollowMode();
+        }
+
         // LateUpdate is called once per frame
-        void LateUpdate()
+        private void LateUpdate()
         {
             _targetPosition = _playerTransform.position + _offset;
+            
+            switch (_followMode)
+            {
+                case FollowMode.Basic:
+                    _targetPosition.y = YPosBase;
+                    break;
+                case FollowMode.ReajustHeight:
+                    _targetPosition.y = _playerTransform.position.y;
+                    break;
+                case FollowMode.GoingDown:
+                    _targetPosition = _playerTransform.position + _offset;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
             _cameraTransform.position = _targetPosition;
+        }
+
+        private FollowMode UpdateFollowMode()
+        {
+            // A REMPLACER PAR VALEUR SOL ET NOUVELLE VALEUR SOL PLUS TARD POUR SADAPTER 
+            if (_playerTransform.position.y > 5)
+            {
+                Vector3.MoveTowards(_cameraTransform.position, _playerTransform.position, Single.MaxValue);
+                return FollowMode.ReajustHeight;
+            }
+
+            if (_playerTransform.position.y < -1)
+            {
+                Vector3.MoveTowards(_cameraTransform.position, _playerTransform.position + _offset, Single.MaxValue);
+                return FollowMode.GoingDown;
+            }
+            return FollowMode.Basic;
         }
     }
 }
--- Assets/_enamis_prototype/Scripts/Player/PlayerController.cs
+++ Assets/_enamis_prototype/Scripts/Player/PlayerController.cs
@@ -5,73 +5,110 @@ namespace _enamis_prototype.Scripts.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        //Constants
-        private const float GravityModifier = 3.5f;
+        // ------Constants ------
+        private const float GravityModifier = 5.0f;
         
-        //References
+        // ------ References ------
         private Rigidbody2D _playerRigidbody2D;
         
-        //Private Attributes
-        private float _speed = 10f;
-        private float _jumpForce = 550.0f;
-
-        [SerializeField] private bool _isOnGround = true;
-
-        private float _horizontalInput;
+        // ------ Private Attributes ------
+        private float _speed = 13f;
+        private float jumpForce = 900.0f;
+        private bool _isOnGround; //Tells if the player is on the ground or not
+        private bool _canJump = true; //In order to prevent jumping with continous pressing
         
-        // Start is called before the first frame update
-        void Start()
+        private float _horizontalInput;
+
+        // ------ Event Methods ------
+
+        private void Awake()
         {
-            Physics2D.gravity *= GravityModifier;
+            //Initializing References
             _playerRigidbody2D = GetComponent<Rigidbody2D>();
         }
 
-        // FixedUpdate is called once per frame
-        void FixedUpdate()
+        // Start is called before the first frame update
+        private void Start()
         {
-            _horizontalInput = Input.GetAxis("Horizontal");
-
-            Move();
-            
-            if (_isOnGround && Input.GetKeyDown(KeyCode.Space))
+            //Initializing Physics Properties
+            Physics2D.gravity *= GravityModifier;
+        }
+        
+        // FixedUpdate is called once per frame
+        private void FixedUpdate()
+        {
+            if (_isOnGround && _canJump &&Input.GetKey(KeyCode.Space))
                 Jump();
         }
 
-        //Basics Movements Methods
+        // Update is called once per frame
+        private void Update()
+        {
+            if (!_canJump && Input.GetKeyUp(KeyCode.Space))
+                _canJump = true;
+            
+            _horizontalInput = Input.GetAxis("Horizontal");
+            Move();
+        }
+        
+        // ------- Basics Movements Methods -------
+        
+        /**
+         * Moves the player in the wanted direction by the user
+         */
         private void Move()
         {
-            Vector3 newAngle = new Vector3(0, 0, -5);
+            Vector3 newAngle = new Vector3(0, 0, -2);
             Transform transform1;
             
             (transform1 = transform).Translate(Vector3.right * (Time.deltaTime * _speed * _horizontalInput));
             transform1.eulerAngles = newAngle * _horizontalInput; //WILL BECOME OBSOLETE AFTER CREATION OF SPRITE ANIMATOR
         }
 
+        /**
+         * Makes the player jump in the air
+         */
         private void Jump()
         {
-            _playerRigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            _playerRigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             StartCoroutine(nameof(JumpAnimationScale));
             _isOnGround = false;
+            _canJump = false;
         }
 
         
-        //Collisions and Triggers Methods
+        // ------- Collisions and Triggers Methods -------
+
         private void OnCollisionEnter2D(Collision2D other)
         {
             if (other.gameObject.CompareTag("Ground"))
                 _isOnGround = true;
         }
-        
-        //Coroutines Methodes
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+                _isOnGround = false;
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Token"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+
+        // ------- Coroutines Methods -------
         
         /**
          * WILL BECOME OBSOLETE AFTER CREATION OF SPRITE ANIMATOR
          */
-        IEnumerator JumpAnimationScale()
+        private IEnumerator JumpAnimationScale()
         {
-            transform.localScale += new Vector3(0, -0.2f, 0);
+            transform.localScale += new Vector3(0, -0.5f, 0);
             yield return new WaitForSeconds(0.1f);
-            transform.localScale += new Vector3(0, 0.2f, 0);
+            transform.localScale += new Vector3(0, 0.5f, 0);
         }
     }
 }
