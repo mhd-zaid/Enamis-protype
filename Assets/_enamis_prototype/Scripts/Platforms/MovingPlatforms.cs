5516294a1dc38a599ecad5add585d6f494f1f6b5
mhd-zaid <zaidmouhamad@gmail.com>
2021-02-12 18:32:35 +0100

    Plateforme mouvante

---
+++ Assets/_enamis_prototype/Scripts/Platforms/MovingPlatforms.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+namespace _enamis_prototype.Scripts.Platforms
+{
+    public class MovingPlatforms : MonoBehaviour
+    {
+        public GameObject platform;
+        public float speed;
+        public Transform currentPosition;
+        public Transform[] points;
+        public int pointSelection;
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentPosition = points[pointSelection];
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPosition.position,
+                speed * Time.deltaTime);
+            if (platform.transform.position == currentPosition.position)
+            {
+                pointSelection++;
+                if (pointSelection == points.Length)
+                {
+                    pointSelection = 0;
+                }
+
+                currentPosition = points[pointSelection];
+            }
+        }
+    }
+}
